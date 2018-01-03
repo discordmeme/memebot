26866ce00feef2319da33c471501fe8be8ef2126
mikeo <foxight@gmail.com>
2018-01-03 17:52:34 +0000

    Update memebot.py

--- memebot.py
+++ memebot.py
@@ -1,28 +1,28 @@
-import discord
-import asyncio
+import discord, asyncio
 
-bot = discord.Client(max_messages=101)
-
+bot = discord.Client(max_messages=100)
 
 @bot.event
 async def on_ready():
     print("Logged in as", bot.user.name, "#", bot.user.discriminator)
-    await bot.change_presence(game=discord.Game(name="discordme.me", type=0))
+    await bot.change_presence(game=discord.Game(name="www.discordme.me", type=0))
     if not discord.opus.is_loaded():
         discord.opus.load_opus("libopus.so")
-
 
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content.lower() == "!nicememe" and message.channel.is_private == False:
-        if message.author.voice.voice_channel != None and message.server.me.voice.voice_channel == None:
+    if message.content.lower() == "!nicememe":
+        if message.channel.is_private:
+            pass
+        elif message.author.voice.voice_channel != None and message.server.me.voice.voice_channel == None:
             voicechannel = message.author.voice.voice_channel
-            if message.server.me.permissions_in(voicechannel).connect and message.server.me.permissions_in(voicechannel).speak:
+            perms = message.server.me.permissions_in(voicechannel)
+            if perms.connect and perms.speak:
                 try:
                     voice = await bot.join_voice_channel(voicechannel)
-                    player = voice.create_ffmpeg_player("/bots/sounds/nice-meme.mp3")
+                    player = voice.create_ffmpeg_player("nicememe.mp3")
                     player.start()
                     while True:
                         if player.is_done():
@@ -36,16 +36,12 @@ async def on_message(message):
             else:
                 if message.channel.permissions_for(message.server.me).send_messages:
                     await bot.send_message(message.channel, "I need the `connect` and `speak` permissions to work!")
-
-    if message.content.lower() == "!invite":
-        if message.channel.permissions_for(message.server.me).send_messages:
-            await bot.send_message(message.channel, "<http://discordmeme.github.io/invite>")
-
+    elif message.content.lower() == "!invite" and (message.channel.is_private or message.channel.permissions_for(message.server.me).send_messages):
+            await bot.send_message(message.channel, "<http://discordme.me/invite>")
     elif message.content.lower().startswith("!eval") and message.author.id == "146726280245673984":
         try:
             await bot.send_message(message.channel, content="```py\n" + str(eval(message.content[6:])) + "```")
         except Exception as e:
             await bot.send_message(message.channel, "```py\n" + repr(e) + "```")
-
 
 bot.run("token")
