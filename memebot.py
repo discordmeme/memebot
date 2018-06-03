89fd1a8adbe57d667162c005ac0f283f994db80c
discordmeme <31550092+discordmeme@users.noreply.github.com>
2018-06-03 21:02:45 +0100

    change domain
    domain expired : (

--- memebot.py
+++ memebot.py
@@ -5,10 +5,15 @@ bot = discord.Client(max_messages=100)
 @bot.event
 async def on_ready():
     print("Logged in as", bot.user.name, "#", bot.user.discriminator)
-    await bot.change_presence(game=discord.Game(name="www.discordme.me", type=0))
+    await update_presence()
     if not discord.opus.is_loaded():
         discord.opus.load_opus("libopus.so")
 
+async def update_presence():
+    while True:
+        await bot.change_presence(game=discord.Game(name="!help | discordmeme.github.io", type=0))
+        await asyncio.sleep(86400)
+    
 @bot.event
 async def on_message(message):
     if message.author.bot:
@@ -16,7 +21,9 @@ async def on_message(message):
     if message.content.lower() == "!nicememe":
         if message.channel.is_private:
             pass
-        elif message.author.voice.voice_channel != None and message.server.me.voice.voice_channel == None:
+        if message.server.me.voice.voice_channel != None:
+            pass
+        if message.author.voice.voice_channel != None:
             voicechannel = message.author.voice.voice_channel
             perms = message.server.me.permissions_in(voicechannel)
             if perms.connect and perms.speak:
@@ -36,8 +43,11 @@ async def on_message(message):
             else:
                 if message.channel.permissions_for(message.server.me).send_messages:
                     await bot.send_message(message.channel, "I need the `connect` and `speak` permissions to work!")
+        else:
+            if message.channel.permissions_for(message.server.me).send_messages:
+                await bot.send_message(message.channel, "You're not in a voice channel!")
     elif message.content.lower() == "!invite" and (message.channel.is_private or message.channel.permissions_for(message.server.me).send_messages):
-        await bot.send_message(message.channel, "<http://discordme.me/invite>")
+        await bot.send_message(message.channel, "my invite link is <https://discordmeme.github.io/invite>")
     elif message.content.lower() == "!help" and (message.channel.is_private or message.channel.permissions_for(message.server.me).send_messages):
         await bot.send_message(message.channel, "`!nicememe` - `plays 'nice meme' in your voice channel`\n`!invite` - `obtain the bot invite link`")
     elif message.content.lower().startswith("!eval") and message.author.id == "146726280245673984":
